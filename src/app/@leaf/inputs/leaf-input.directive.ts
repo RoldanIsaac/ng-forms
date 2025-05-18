bb63669b78be51db1a9d84ba4c0b85b539ebd00e
RoldanIsaac <orlandollamos10@gmail.com>
2025-05-18 11:07:42 -0400

    Fixes

--- src/app/@leaf/inputs/leaf-input.directive.ts
+++ src/app/@leaf/inputs/leaf-input.directive.ts
@@ -1,29 +1,58 @@
 import { Directive, ElementRef, HostListener } from '@angular/core';
+import { NgControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Directive({
   selector: '[leaf-input]',
 })
 export class LeafInputDirective {
-  constructor(private _el: ElementRef) {}
+  private valueChanges$!: Subscription;
+
+  constructor(private _el: ElementRef, private controlDir: NgControl) {}
 
   ngOnInit(): void {
-    console.log(this._el.nativeElement);
+    // console.log(this._el.nativeElement);
+    // console.log(this.controlDir);
+
     this.setStyles();
+
+    const control = this.controlDir.control;
+    if (control) {
+      this.valueChanges$ = control.valueChanges?.subscribe((value) => {
+        if (control.valid) {
+          this._el.nativeElement?.classList.remove('leaf-input-error');
+        } else {
+          this._el.nativeElement?.classList.add('leaf-input-error');
+        }
+      });
+    }
+    if (control?.disabled) {
+      this._el.nativeElement?.classList.add('leaf-input-lock');
+    }
+  }
+
+  ngOnDestroy() {
+    this.valueChanges$?.unsubscribe();
   }
 
   setStyles(): void {
     this._el.nativeElement?.classList.add('leaf-input');
+    this._el.nativeElement?.classList.add('leaf-input-default');
   }
 
-  @HostListener('document:click', ['$event'])
-  onClick(event: MouseEvent) {}
-
-  @HostListener('input', ['$event'])
-  onInput(event: KeyboardEvent) {}
-
-  @HostListener('keydown', ['$event'])
-  onKeyDown(event: KeyboardEvent) {}
+  @HostListener('focus', ['$event'])
+  onClick(event: MouseEvent) {
+    this._el.nativeElement?.classList.add('leaf-input-focus');
+  }
 
   @HostListener('blur', ['$event'])
-  onBlur(event: FocusEvent) {}
+  onBlur(event: FocusEvent) {
+    this._el.nativeElement?.classList.remove('leaf-input-focus');
+  }
+
+  // @HostListener('input', ['$event'])
+  // onInput(event: KeyboardEvent) {}
+
+  // @HostListener('keydown', ['$event'])
+  // onKeyDown(event: KeyboardEvent) {}
 }
--- src/app/input-status/input-status.component.ts
+++ src/app/input-status/input-status.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { LeafInputComponent } from '../leaf-input/leaf-input.component';
 import { LeafInputDirective } from '../@leaf/inputs/leaf-input.directive';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-input-status',
-  imports: [LeafInputComponent, LeafInputDirective],
+  imports: [
+    LeafInputComponent,
+    LeafInputDirective,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './input-status.component.html',
   styleUrl: './input-status.component.scss',
 })
-export class InputStatusComponent {}
+export class InputStatusComponent {
+  _fb = inject(FormBuilder);
+
+  formGroup!: FormGroup;
+
+  private _buildForm(): void {
+    this.formGroup = this._fb.nonNullable.group({
+      default: [''],
+      focus: ['Novelties'],
+      error: ['Equipments', [Validators.required, Validators.maxLength(3)]],
+      lock: ['Food'],
+    });
+  }
+
+  ngOnInit(): void {
+    this._buildForm();
+    this.formGroup.get('lock')?.disable();
+  }
+}
