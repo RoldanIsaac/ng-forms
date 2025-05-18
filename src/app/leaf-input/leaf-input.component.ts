666bfced059056d887477427689abcf179220350
RoldanIsaac <orlandollamos10@gmail.com>
2025-05-18 12:40:48 -0400

    Fixes

--- src/app/leaf-input/leaf-input.component.ts
+++ src/app/leaf-input/leaf-input.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'lf-input',
+  selector: 'leaf-input',
   imports: [],
   templateUrl: './leaf-input.component.html',
   styleUrl: './leaf-input.component.scss',
